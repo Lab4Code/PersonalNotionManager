fc37ed500ddf85ae25e61b48f632ad22772d76fe
Maximilian Mauroner <maximilian@mauroner.eu>
2022-09-08 23:01:19 +0200

    working instapaper implementation, now all that's needed is the syncing, and the saving of the data

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,26 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import NextAuth, {type NextAuthOptions} from "next-auth";
 
-// Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
-import { env } from "../../../env/server.mjs";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {prisma} from "../../../server/db/client";
+import {env} from "../../../env/server.mjs";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
+    callbacks: {
+        session({session, user}) {
+            if (session.user) {
+                session.user.id = user.id;
+            }
+            return session;
+        },
     },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GitHubProvider({
+            clientId: env.GITHUB_CLIENT_ID,
+            clientSecret: env.GITHUB_CLIENT_SECRET,
+        }),
+    ],
 };
 
 export default NextAuth(authOptions);
